9e0cd0c67d51c62dfdd6fc0f4d85141cbbda6aa5
Gurpranked <gurpreet.singh.rehal@outlook.com>
2026-02-27 17:14:50 -0500

    feat: Created template for provisioning workspaces with your own repo

---
+++ templates/k8s-pod-custom-repo/README.tf
@@ -0,0 +1,14 @@
+---
+display_name: Kubernetes Pod Workspace (provide repo)
+description: Provision Kubernetes Deployments as Coder workspaces
+icon: ../../../site/static/icon/k8s.png
+maintainer_github: UMLCloudComputing
+verified: true
+tags: [kubernetes, container]
+---
+
+# Remote Development on Kubernetes Pods
+
+Provision Kubernetes Pods as [Coder workspaces](https://coder.com/docs/workspaces).
+
+Provide your own GitHub repository URL to clone.
